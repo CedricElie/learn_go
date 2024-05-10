a2150309b36cf566cc5116761de9c56a2012f9f9
CedricElie <cedstylz@gmail.com>
2024-05-10 17:48:52 +0200

    Variables

--- main.go
+++ main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+//this is a comment
+
+/*
+this is a
+multi line
+comment
+*/
+
 func main() {
 	fmt.Println("Hello World!")
+
+	name := "Lisa"
+	fmt.Println(name)
+
+	var greeting string = "Hello World"
+	fmt.Println(greeting)
+
+	var lab string = "GoDojo"
+	var user string = "CK"
+
+	fmt.Print("Welcome to ", lab, ", ", user)
+
+	// Format specifiers
+	var joe string = "Joe"
+	var i int = 78
+	fmt.Printf("Hey, %v! You have scored %d/100 in Physics", joe, i)
+
 }
