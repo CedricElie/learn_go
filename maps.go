fef50a8549f53b93e0eef685e7d3356f450e7c91
CedricElie <cedstylz@gmail.com>
2024-06-06 01:27:12 +0200

    maps

---
+++ maps.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Maps")
+	codes := map[string]string{"en": "English", "fr": "French", "hi": "Hindi"}
+
+	fmt.Println(codes)
+
+	// Declare and initializing a map - make function
+}
